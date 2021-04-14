618cf72aa688c2ca2fe7babbabe509eb3d9a4048
JessIxAm <franknkongo@protonmail.com>
2021-04-14 13:23:27 +0200

    main.js and styles.css added and edited

---
+++ main.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.getElementById('dice_player1').setAttribute('src', `assets/dice${randomNumber1}.png`);
+document.getElementById('dice_player2').setAttribute('src', `assets/dice${randomNumber2}.png`);
+
+var winner = document.getElementById('winner');
+
+     if (randomNumber1 > randomNumber2) {
+          winner.setAttribute('class', 'wrinkle');
+          winner.innerHTML = "Player 1 Wins !!!";
+     } else if (randomNumber1 < randomNumber2) {
+          winner.setAttribute('class', 'wrinkle');
+          winner.innerHTML = "Player 2 Wins !!!";
+     } else {
+          winner.setAttribute('class', '');
+          winner.innerHTML = "No one wins, try again";
+     }
